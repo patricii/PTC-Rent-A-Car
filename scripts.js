da6305df89419c128df3b9041c8933cd3c27c9a2
jagapatr <jagapatr@motorola.com>
2021-04-14 12:21:32 -0300

    Filtro adicionado

--- scripts.js
+++ scripts.js
@@ -3,16 +3,18 @@ function pesquisar(){
     var input, filtro, menu, menuItens, links;
 
     input = document.getElementById("pesquisa");
-    filtro = input.nodeValue.toUpperCase();
+    filtro = input.value.toUpperCase();
     menu = document.getElementById("catalogo");
     menuItens = menu.getElementsByClassName("card-content");
 
     for(var i = 0 ; i <= menuItens.length ; i++)
     {
-        links = getElementsByTagName("h3");
-        if(links.innerHTML.toUpperCase().indexOf(filtro)){
-
+        links = menuItens[i].getElementsByTagName("h3")[0];
+        if(links.innerHTML.toUpperCase().indexOf(filtro) >-1){
+            menuItens[i].style.display = "";
         }
-
+        else{
+            menuItens[i].style.display = "none";
+        }
     }
 }
